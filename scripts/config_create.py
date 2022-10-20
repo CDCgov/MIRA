51948c39388d5460ebb96d9c19a9a9d5e887cf34
Kristine Lacek <qgx6@cdc.gov>
2022-10-20 12:11:49 -0400

    simplified samplesheet, config creation

--- scripts/config_create.py
+++ scripts/config_create.py
@@ -8,14 +8,14 @@ import subprocess
 root = '/'.join(abspath(__file__).split('/')[:-2])
 
 if len(argv) < 1:
-        exit('\n\tUSAGE: {} <samplesheet.csv> <machine_path> <runid>\n'.format(__file__))
+        exit('\n\tUSAGE: {} <samplesheet.csv> <runpath> \n'.format(__file__))
 
 try:
-        machine,runid = argv[2:4]
+        runpath = argv[2]
 except (IndexError, ValueError):
-        machine,runid = 'testMachine','testRunID'
+        runid = 'testRunID'
 
-data = {'runid':runid, 'machine':machine, 'barcodes':{}}
+data = {'runid':runpath.split('/')[runpath.split('/').index('fastq_pass') -1], 'barcodes':{}}
 def reverse_complement(seq):
     rev = {'A':'T','T':'A','C':'G','G':'C',',':','}
     seq = seq[::-1]
@@ -32,12 +32,11 @@ except:
     with open('{}/lib/{}.yaml'.format(root, dfd[0]['Barcode Expansion Pack']), 'r') as y:
         barseqs = yaml.safe_load(y)
 for d in dfd.values():
-    fastq_pass = glob(machine + '/' + runid + '/*/*' + d['Flow Cell ID'] + "*/fastq_pass/*/")
+    fastq_pass = glob(runpath + '/*/')
     if d['Barcode #'] in [x.split("/")[-2] for x in fastq_pass]:
 
         data['barcodes'][d['Sample ID']] = {
-                                                                        'flow_cell_id':d['Flow Cell ID'],
-                                                                        'flow_cell_product_code':d['Flow Cell Product Code'],
+                                                                        
                                                                         'sample_type':d['Sample Type'],
                                                                         'barcode_number':d['Barcode #'],
                                                                         'barcode_sequence':barseqs[d['Barcode #']],
