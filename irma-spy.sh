353459a63905c0447178184381922fd01ecfdb52
Ben Rambo-Martin <nbx0@cdc.gov>
2022-11-01 10:49:56 -0400

    trying to merge to container_init

--- irma-spy.sh
+++ irma-spy.sh
@@ -3,5 +3,5 @@
 # Wrapper to launch irma-spy dashboard
 
 RESOURCE_ROOT=/irma-spy
-[ $1 == 'dev' ] && YAML="container-dev.yaml" || YAML="container-prod.yaml"
-python $RESOURCE_ROOT/app.py $RESOURCE_ROOT/config/$YAML 
+
+python $RESOURCE_ROOT/app.py $RESOURCE_ROOT/config/container.yaml
