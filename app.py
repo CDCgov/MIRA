6efd50059d864d8a723feab6c8ba5dad13d1c79f
kristinelacek <qgx6@cdc.gov>
2024-10-18 11:01:47 -0400

    comma bug

--- app.py
+++ app.py
@@ -1057,11 +1057,11 @@ content = html.Div(
                     {"label": "Flu-ONT", "value": "Flu-ONT"},
                     {"label": "Flu-Illumina", "value": "Flu-Illumina"},
                     {"label": "SC2-Spike-Only-ONT", "value": "SC2-Spike-Only-ONT"},
-                    {"label": "SC2-Spike-Only-Illumina", "value":"SC2-Spike-Only-Illumina"}
+                    {"label": "SC2-Spike-Only-Illumina", "value":"SC2-Spike-Only-Illumina"},
                     {"label": "SC2-Whole-Genome-ONT", "value":"SC2-Whole-Genome-ONT"},
                     {"label": "SC2-Whole-Genome-Illumina", "value": "SC2-Whole-Genome-Illumina"},
                     {"label": "RSV-Illumina", "value": "RSV-Illumina"},
-                    {"label": "RSV-ONT", "value": "RSV-ONT",},
+                    {"label": "RSV-ONT", "value": "RSV-ONT"},
                 ],
                 id="experiment_type",
                 placeholder="What kind of data is this?",
